638b7571c98cb4823b8a01317f659fc536fbf9f0
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-15 14:58:41 -0600

    Signos

---
+++ 13Signos/app.js
@@ -0,0 +1,5 @@
+let dia=document.querySelector("#dia");
+let vdia=document.querySelector("#vdia");
+let mes=document.querySelector("#mes");
+let fecha=document.querySelector("#fecha");
+let sp=document.querySelector("#spinner");
