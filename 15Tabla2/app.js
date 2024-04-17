33f51cdabf6ae3b46a16b6efce1abc455c0b6279
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-17 11:55:07 -0600

    h

--- 15Tabla2/app.js
+++ 15Tabla2/app.js
@@ -1,41 +1,41 @@
-let fi=document.querySelector("#fi");
-let vfi=document.querySelector("#vfi");
-let co=document.querySelector("#co");
-let vco=document.querySelector("#vco");
-let ta=document.querySelector("#ta");
-let c1=document.querySelector("#c1");
-let c2=document.querySelector("#c2");
-
+let fi = document.querySelector("#fi");
+let vfi = document.querySelector("#vfi");
+let co = document.querySelector("#co");
+let vco = document.querySelector("#vco");
+let ta = document.querySelector("#ta");
+let c1 = document.querySelector("#c1");
+let c2 = document.querySelector("#c2");
 
 vfi.innerHTML = fi.value;
 vco.innerHTML = co.value;
 
-fi.oninput = () => { editar() }
-co.oninput = () => { editar() }
+fi.oninput = () => { editar() };
+co.oninput = () => { editar() };
+c1.onchange = () => { editar() };
+c2.onchange = () => { editar() };
 
 const editar = () => {
-    let f= parseInt(fi.value);
-    let c= parseInt(co.value);
+    let f = parseInt(fi.value);
+    let c = parseInt(co.value);
 
-    vfi.innerHTML=f;
-    vco.innerHTML=c;
+    vfi.innerHTML = f;
+    vco.innerHTML = c;
 
     tabla(f, c);
 }
 
 const tabla = (fi, co) => {
-    let table= '';
+    let table = '';
 
-   for(let o=0; o<fi ; o++){
-    table+='<tr>';
+    for (let o = 0; o < fi; o++) {
+        let rowColor = o % 2 === 0 ? c1.value : c2.value; 
+        table += `<tr style="background-color: ${rowColor};">`;
 
-
-    for(let p=0; p<co; p++){
-        table+='<td></td>';   
+        for (let p = 0; p < co; p++) {
+            table += '<td></td>';
+        }
+        table += '</tr>';
     }
-    table+='</tr>';
-   }
 
     ta.innerHTML = table;
-
 };
