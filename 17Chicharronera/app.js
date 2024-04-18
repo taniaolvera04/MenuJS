008670445f6cbcfbf56abda074206c26f77a698c
Tania Olvera <160749892+taniaolvera04@users.noreply.github.com>
2024-04-18 11:37:50 -0600

    Update app.js

--- 17Chicharronera/app.js
+++ 17Chicharronera/app.js
@@ -6,11 +6,17 @@ let ca = document.querySelector("#ca");
 let cb = document.querySelector("#cb");
 let cc = document.querySelector("#cc");
 
+let chi = document.querySelector("#chi");
+
+let ec= document.querySelector("#ec");
 let r = document.querySelector("#r");
+
+chi.innerHTML="<img src='chi.png' width=200>";
 
 va.innerHTML = ca.value;
 vb.innerHTML = cb.value;
 vc.innerHTML = cc.value;
+
 
 ca.oninput = () => { calcular() };
 cb.oninput = () => { calcular() };
@@ -30,15 +36,20 @@ const calcular = () => {
         return;
     }
 
+ec.innerHTML=a+"x² + "+b+"x + "+c+" = 0";
+
     if(a===0){
         r.innerHTML="No es una ecuación cuadrática válida";
         return;
     }
+    
+    
+
+let factor = Math.pow(b, 2) - (4 * a * c);
 
     r.innerHTML="<img src='sp.gif' width=90>";
     setTimeout(()=>{
 
-    let factor = Math.pow(b, 2) - (4 * a * c);
 
     if (factor === 0) {
         let x = (-b) / (2 * a);
@@ -46,10 +57,9 @@ const calcular = () => {
     } else if (factor > 0) {
         let x1 = (-b + Math.sqrt(factor)) / (2 * a);
         let x2 = (-b - Math.sqrt(factor)) / (2 * a);
-        r.innerHTML = "x1= " + x1.toFixed(2) + "<br><br>" + "x2= " + x2.toFixed(2);
+        r.innerHTML = "x1= " + x1.toFixed(2) + "<br>" + "x2= " + x2.toFixed(2);
     } else {
         r.innerHTML = "No hay solución";
     }
 }, 300);
 }
-
