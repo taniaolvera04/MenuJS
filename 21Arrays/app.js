87a642f3504e65721fddae558d620219b17c30c2
Tania Olvera <160749892+taniaolvera04@users.noreply.github.com>
2024-04-23 10:59:18 -0600

    Update app.js

--- 21Arrays/app.js
+++ 21Arrays/app.js
@@ -1,1 +1,27 @@
+var btnAdd=document.getElementById("btnAdd");
+var btnDel=document.getElementById("btnDel");
+var items=document.getElementById("items");
+ var miarray=new Array();
 
+btnAdd.onclick=()=>{
+  var n=parseInt(document.getElementById("item").value);
+  miarray.push(n);
+  var nums="";
+  for(var i=0; i<miarray.length; i++){
+    nums+="<h2>"+miarray[i]+"</h2>";
+  }
+  items.innerHTML=nums;
+
+
+  
+  btnDel.onclick=()=>{
+    var n=parseInt(document.getElementById("item").value);
+  miarray.pop(n);
+  var nums="";
+  for(var i=0; i<miarray.length; i++){
+    nums+="<h2>"+miarray[i]+"</h2>";
+  }
+  items.innerHTML=nums;
+
+  }
+}
