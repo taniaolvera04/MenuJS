995423953575d10a89c4102f8e7092166d9dece9
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-23 11:54:41 -0600

    Arrays

--- 21Arrays/app.js
+++ 21Arrays/app.js
@@ -1,7 +1,77 @@
 var btnAdd=document.getElementById("btnAdd");
 var btnDel=document.getElementById("btnDel");
 var items=document.getElementById("items");
+var unshift=document.getElementById("un");
+var shift=document.getElementById("shi");
+var index=document.getElementById("btnIndex");
+var splice=document.getElementById("splice");
+
  var miarray=new Array();
+
+splice.onclick=()=>{
+  const {value: pos}=await Swal.fire({
+    title: "SPLICE",
+    input: "text",
+    inputLabel: "¿Qué posición inicial?",
+    showCancelButton: true,
+    inputValidator: (value)=>{
+      if(!value){
+        return "Ingresa valor válido";
+      }
+    }
+  });
+const {value: n}=await Swal.fire({
+  title:"SPLICE",
+  input: text,
+  inputLabel
+})
+
+  try{
+    miarray.splice(pos,n);
+    imprimirArray();
+  }catch(error){
+    Swal.fire({
+      title: "SPLICE",
+      text: "No se pudo eliminar",
+      icon:"error"
+    })
+  }
+}
+
+
+
+
+index.onclick=()=>{
+  let pos=miarray.indexOf(parseInt(document.getElementById("index").value));
+  Swal.fire({
+    title: "ARRAY",
+    text: "Posición: "+pos,
+    icon:"success"
+  })
+}
+
+
+ shift.onclick=()=>{
+  n=parseInt(document.getElementById("item").value);
+  miarray.shift(n);
+  imprimirArray()
+}
+
+
+unshift.onclick=()=>{
+  n=parseInt(document.getElementById("item").value);
+  miarray.unshift(n);
+  imprimirArray()
+}
+
+const imprimirArray=()=>{
+  let x="";
+  for(let i=0; i<miarray.length; i++){
+    x+=`<h3>${miarray[i]}</h3>`;
+  }
+  items.innerHTML=x;
+}
+
 
 btnAdd.onclick=()=>{
   var n=parseInt(document.getElementById("item").value);
