bd0126278dc09f837756e70df76be49f30afe8e2
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-24 11:58:48 -0600

    cambios

--- 22Tiendita/app.js
+++ 22Tiendita/app.js
@@ -4,8 +4,13 @@ var precios=[23, 18, 17, 20, 7];
 var selectProductos=document.getElementById("productos");
 var imgProductos=document.getElementById("imgProducto");
 var precioProductos=document.getElementById("precioProductos");
-
+var inputCantidad=document.getElementById("inputCantidad");
+var agregarCarrito=document.getElementById("agregarCarrito");
 var carrito=new Array();
+
+var posProducto=-1;
+var cantidadProducto=0;
+
 
 const cargarProductos=()=>{
     let optionProductos="";
@@ -26,12 +31,50 @@ const cargarPrecio=()=>{
     posProducto=selectProductos.selectedIndex;
 }
 
-
+inputCantidad.oninput=()=>{
+    document.getElementById("vcantidad").innerHTML=inputCantidad.value;
+    cantidadProducto=parseInt(inputCantidad.value);
+}
 
 agregarCarrito.onclick=()=>{
-    let divCarrito=doccument.getElementById("carrito");
+    cantidadProducto=parseInt(inputCantidad.value);
     let item=new Array();
     item.push(posProducto);
-    item.push(posProducto);
+    item.push(cantidadProducto);
     carrito.push(item);
+    imprimirTabla();
+}
+
+const imprimirTabla=()=>{
+    let total=0;
+    let divCarrito=document.getElementById("carrito");
+    let tablaHTML=`<table class="table w-100 m-auto">
+        <tr>
+        <td>PRODUCTO</td>
+        <td>PRECIO</td>
+        <td>CANTIDAD</td>
+        <td>IMPORTE</td>
+        </tr>
+        `;
+        carrito.forEach(item=>{
+            tablaHTML+=`
+            <tr>
+            <td>${productos[item[0]]}</td>
+            <td>$ ${precios[item[0]]}.00</td>
+            <td>${item[1]}</td>
+            <td>${(precios[item[0]]*item[1])}</td>
+            </tr>
+            `
+            total+=(precios[item[0]]*item[1]);
+    })
+    tablaHTML+=`
+    <tr>
+    <td></td>
+    <td></td>
+    <td><h3>TOTAL</h3></td>
+    <td><h3>$${total}.00</h3></td>
+    </tr>
+    `
+
+    divCarrito.innerHTML=tablaHTML;
 }
