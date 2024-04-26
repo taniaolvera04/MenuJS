e8eae6990b756ae964e293dc010cf613e0e21f73
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-26 11:45:18 -0600

    cambios

--- 22Tiendita/app.js
+++ 22Tiendita/app.js
@@ -160,3 +160,30 @@ if (pago>= totalp) {
   }
 });
         }
+
+
+const verProductos=()=>{
+  let divListaProductos=document.getElementById("listaProductos");
+  let tablaHTML=
+  `<table class="table w-100 m-auto">
+      <tr>
+      <td>PRODUCTO</td>
+      <td>PRECIO</td>
+      <td>DEL</td>
+      </tr>
+      `;
+      let index=0;
+      productos.forEach(item=>{
+          tablaHTML+=`
+          <tr>
+          <td>${item}</td>
+          <td>$ ${precios[index]}.00</td>
+          <td><button class="btn btn-danger" onclick="delProductos(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+    <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+  </svg></button></td>
+          </tr>
+          `
+          index++;
+  })
+  divListaProductos.innerHTML=tablaHTML;
+}
