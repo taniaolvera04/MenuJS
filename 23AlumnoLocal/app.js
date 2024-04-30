d54a2cc5ac82572dd226dfd38fca76c03fc0cce3
Tania <oert070904mmclyna2@soycecytem.mx>
2024-04-29 21:23:40 -0600

    Alumno

--- 23AlumnoLocal/app.js
+++ 23AlumnoLocal/app.js
@@ -20,18 +20,18 @@ var c = document.getElementById("c").value;
 
 if(nombre.trim()=="" ||ap.trim()==""||am.trim()==""){
   Swal.fire({
-    title:"ALUMNOS",
-    text:"CAMPOS VACÍOS",
+    title:"Alumnos",
+    text:"Tienes campos vacíos, ingresa valores válidos",
     icon:"error"
   });
   return;
 }
 
-let alumno={nombre, ap,am,g,c};
+let alumno={nombre,ap,am,g,c};
 alumnos.push(alumno);
   localStorage.setItem("alumnos", JSON.stringify(alumnos));
-  limpiarForm();
   imprimirAlumnos();
+  limpiarForm();
   
 }
 
@@ -46,6 +46,7 @@ const imprimirAlumnos = () =>{
       <td>Grupo</td>
       <td>Carrera</td>
       <td>DEL</td>
+      <td>Editar</td>
       </tr>
       `;     
       let index=0;       
@@ -60,7 +61,7 @@ const imprimirAlumnos = () =>{
     <td><button class="btn btn-danger" onclick="delAlumnos(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
 </svg></button></td>
-<td><button class="btn btn-info" onclick="editarAlumnos(${index})" data-bs-toggle="modal" data-bs-target="#editar">
+<td><button class="btn btn-info" onclick="mostrarAlumnos(${index})" data-bs-toggle="modal" data-bs-target="#actualizarAlumnos">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-pencil-fill" viewBox="0 0 16 16">
 <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
 </svg></button></td>
@@ -94,12 +95,10 @@ borrar.onclick=()=>{
   
 }
 
-
-
 delAlumnos=(index)=>{
   
     Swal.fire({
-      title: "¿Estás seguro de eliminar este alumno?",
+      title: "¿Estás seguro de eliminar a este alumno?",
       showDenyButton: true,
       confirmButtonText: "Si, eliminar",
       denyButtonText: "No estoy seguro"
@@ -115,3 +114,33 @@ delAlumnos=(index)=>{
       }
     });
   }
+
+  var indiceAlumno; 
+  function mostrarAlumnos(index) {
+    indiceAlumno=index;
+      var alumnos = JSON.parse(localStorage.getItem("alumnos"));
+      var alumno = alumnos[index];
+  
+      document.getElementById("nom").value = alumno.nombre;
+      document.getElementById("apa").value = alumno.ap;
+      document.getElementById("ama").value = alumno.am;
+      document.getElementById("gr").value = alumno.g;
+      document.getElementById("ca").value = alumno.c;
+      document.getElementById("actualizarAlumnos").style.display = "block"; 
+      //establece el estilo display del elemento modal con id actualizarAlumnos en "block", lo que lo hace visible en la página.
+  }
+  
+  actualizar.onclick = () => {
+      var alumnos = JSON.parse(localStorage.getItem("alumnos"));
+      var alumno = alumnos[indiceAlumno]; 
+
+      alumno.nombre = document.getElementById("nom").value;
+      alumno.ap = document.getElementById("apa").value;
+      alumno.am = document.getElementById("ama").value;
+      alumno.g = document.getElementById("gr").value;
+      alumno.c = document.getElementById("ca").value;
+  
+      localStorage.setItem("alumnos", JSON.stringify(alumnos));
+      imprimirAlumnos();
+  }
+  
